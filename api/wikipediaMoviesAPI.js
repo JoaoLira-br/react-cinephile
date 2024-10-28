cd9cb368bfe771c8e083ba33a84ec9fa90d163ad
Joao Victor Lira <lira440440@gmail.com>
2024-10-28 17:40:46 -0300

    adds movies backend

---
+++ api/wikipediaMoviesAPI.js
@@ -0,0 +1,29 @@
+import movies1970 from './movies-1970s.json';
+import movies1980 from './movies-1980s.json';
+import movies1990 from './movies-1990s.json';
+import movies2000 from './movies-2000s.json';
+import movies2010 from './movies-2010s.json';
+import movies2020 from './movies-2020s.json';
+
+export const fetchMoviesByGenre = (genreString) => {
+    // console.log(`genreString: ${genreString}`);
+
+    const allMovies = [
+        // ...movies1970,
+        // ...movies1980,
+        // ...movies1990,
+        ...movies2000,
+        ...movies2010,
+        ...movies2020
+    ];
+
+    const genres = genreString.split(',').map(genre => genre.trim().toLowerCase());
+
+    const filteredMovies = allMovies.filter(movie => 
+        movie.genres.some(movieGenre => genres.includes(movieGenre.toLowerCase()))
+    );
+
+    const shuffledMovies = filteredMovies.sort(() => Math.random() - 0.5);
+    
+    return shuffledMovies;
+}
